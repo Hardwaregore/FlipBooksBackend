4bd79a4f80b32cabd0c88a0ddcf16ec322845825
Hardwaregore <hardwaregore101@gmail.com>
2024-01-16 21:18:54 -0600

    Added auth and the create phone function

--- index.js
+++ index.js
@@ -5,6 +5,30 @@ const PORT = 3547;
 
 flipBooks.use(express.json())
 
+// sD&wwHJ6o8o&Jciu8!o4C&s65fLmJ&94fPm#sCvwS&D%mpibLXEog!Q&xJ@sq96GD7oxNjLwuQ#WCfv#@GP7Jz**L7RT8^5$zW%&tU&gEWbMVqE%aB$EeU5SjMgVacfA
+flipBooks.use((req, res, next) => {
+    conn.query("SELECT * FROM `keys`", function (err, result) {
+        if (err) {
+            console.log("Uh-oh, the SQL server is f̶u̶*̶k̶e̶d̶ sad");
+            throw err;
+        };
+        const apiKey = req.headers['authorization']; // Assuming the API key is sent in the 'x-api-key' header
+    
+        var expectedKey = result[0].apiKey;
+        expectedKey = "Bearer " + expectedKey;
+    
+        if (apiKey && apiKey === expectedKey) {
+            console.log('API Key Authentication Successful');
+            next();
+        } else {
+            console.log('API Key Authentication Failed');
+            res.status(401).send({ message: 'Lol you cant do that, so we f̶u̶*̶k̶e̶d̶ discarded your request for you' });
+        }
+    });
+
+
+});
+
 var sql = require('mysql');
 
 var username = process.env.MYSQL_USER;
@@ -20,7 +44,7 @@ var conn = sql.createConnection({
 try {
     flipBooks.listen (PORT)
 } catch(e) {
-    console.log("Oh no, this server is f̶u̶*̶k̶e̶d̶ sad");
+    console.log("Oh no, the server is f̶u̶*̶k̶e̶d̶ sad");
     process.exit();
 } finally {
     console.log("The FlipBooks Server is now Ready!");
@@ -39,41 +63,57 @@ flipBooks.get('/phone', (req, res) => {
         if (err) {
             console.log("Uh-oh, the SQL server is f̶u̶*̶k̶e̶d̶ sad");
             throw err;
-            process.exit();
         };
         console.log("Query Seccessful!", result);
         // var json = JSON.parse(result);
         res.status(200).send(result);
     });
-    // id, phone, storage, color, purchasedFor, repairs, repairCost, sellFor, profit, sold
-    /*
-        [
-            RowDataPacket {
-                id: 1,
-                phone: 'iPhone 12 Pro Max',
-                storage: 256,
-                color: 'Space Gray',
-                purchasedFor: 300,
-                repairs: 'Screen Refurbish',
-                repairCost: 50,
-                sellFor: 400,
-                profit: 50,
-                sold: 0
-            },
-            RowDataPacket {
-                id: 2,
-                phone: 'iPhone 13 Pro',
-                storage: 128,
-                color: 'Product Red',
-                purchasedFor: 310,
-                repairs: 'Screen Refurbish',
-                repairCost: 50,
-                sellFor: 400,
-                profit: 40,
-                sold: 0
-            }
-        ]
-    */
 
 });
 
+flipBooks.get('/phone/:id', (req, res) => {
+    var id = req.params.id;
+    // check if id is valid
+    
+    conn.query("SELECT * FROM `phones` WHERE `id` = " + id, function (err, result) {
+        if (err) {
+            console.log("Uh-oh, the SQL server is f̶u̶*̶k̶e̶d̶ sad");
+            throw err;
+        };
+        if (result.length == 0) {
+            res.status(404).send({message: "Uh-Oh, Phone #" + id + " is completely f̶u̶*̶k̶e̶d̶ sad"});
+            return;
+        }
+        console.log("Query Seccessful!", result);
+        res.status(200).send(result);
+    });
+
+})
+
+flipBooks.post('/phone', (req, res) => {
+    var id = req.body.id;
+    var phone = req.body.phone;
+    var storage = req.body.storage;
+    var color = req.body.color;
+    var purchasedFor = req.body.purchasedFor;
+    var repairs = req.body.repairs;
+    var repairCost = req.body.repairCost;
+    var sellFor = req.body.sellFor;
+    var profit = sellFor - purchasedFor - repairCost;
+    var sold = req.body.sold;
+
+    if (id == undefined || phone == undefined || storage == undefined || color == undefined || purchasedFor == undefined || repairs == undefined || repairCost == undefined || sellFor == undefined || sold == undefined) {
+        res.status(400).send({message: "Uh-oh, your request is completely f̶u̶*̶k̶e̶d̶ sad"});
+        return;
+    }
+
+
+    conn.query("INSERT INTO `phones` (`id`, `phone`, `storage`, `color`, `purchasedFor`, `repairs`, `repairCost`, `sellFor`, `profit`, `sold`) VALUES ('" + id + "', '" + phone + "', '" + storage + "', '" + color + "', '" + purchasedFor + "', '" + repairs + "', '" + repairCost + "', '" + sellFor + "', '" + profit + "', '" + sold + "')", function (err, result) {
+        if (err) {
+            console.log("Uh-oh, the SQL server is f̶u̶*̶k̶e̶d̶ sad");
+            throw err;
+        };
+        res.status(200).send();
+        
+    });
+})
--- init.sql
+++ init.sql
@@ -14,5 +14,14 @@ CREATE TABLE `phones` (
     `sold` BOOLEAN DEFAULT FALSE
 );
 
+CREATE TABLE `keys` (
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `apiKey` VARCHAR(200) NOT NULL
+);
+
+INSERT INTO `keys` (`apiKey`) VALUES ('sD&wwHJ6o8o&Jciu8!o4C&s65fLmJ&94fPm#sCvwS&D%mpibLXEog!Q&xJ@sq96GD7oxNjLwuQ#WCfv#@GP7Jz**L7RT8^5$zW%&tU&gEWbMVqE%aB$EeU5SjMgVacfA');
+
+-- Test Data:
+
 INSERT INTO `phones` (`phone`, `storage`, `color`, `purchasedFor`, `repairs`, `repairCost`, `sellFor`, `profit`) VALUES ('iPhone 12 Pro Max', 256, 'Space Gray', 300, 'Screen Refurbish', 50, 400, 50);
 INSERT INTO `phones` (`phone`, `storage`, `color`, `purchasedFor`, `repairs`, `repairCost`, `sellFor`, `profit`) VALUES ('iPhone 13 Pro', 128, 'Product Red', 310, 'Screen Refurbish', 50, 400, 40);
